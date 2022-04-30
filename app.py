33c391ad8f79b62abc8ffd150061d1e7561eec4e
WilliamYKZ <63417904+WilliamYKZ@users.noreply.github.com>
2022-04-30 03:42:24 -0500

    Update app.py
    Adding a paint method

--- app.py
+++ app.py
@@ -208,3 +208,13 @@ def reset_maze_state():
     if not maze_state.is_empty():
         maze_state.reset()
     return 'OK', 200
+
+# Route for painting a cell
+@app.route('/paint/<coord>/<color>', methods=["PUT"])
+def paint(coord, color):
+  if((color is not tuple) and (0<= color[0] <=255) 
+     and (0<= color[1] <=255) and (0<= color[2] <=255)):
+    if (cells.get(coord)!=None):
+      colors[coord] = color
+      return "OK",200
+  return "Color or cell not found", 404
